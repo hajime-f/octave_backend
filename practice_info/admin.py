b4189b1cac1fbf6569badf5faaf78aab2308cc67
Hajime Fujita <hajime.fujita@gmail.com>
2021-02-09 13:36:03 +0000

    PracticeInfoを作成

--- practice_info/admin.py
+++ practice_info/admin.py
@@ -1,3 +1,14 @@
+from .models import Orchestra
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+class PracticeInfoAdmin(admin.ModelAdmin):
+
+    fieldsets = (
+        (_('Practice info'), {'fields': ('title', 'content', 'orchestra')}),
+    )
+    list_display = ('title', 'content', 'orchestra',)
+    search_fields = ('title', 'content', 'orchestra',)
+    ordering = ('created_at',)
+    
+admin.site.register(PracticeInfo, PracticeInfoAdmin)
--- practice_info/models.py
+++ practice_info/models.py
@@ -1,3 +1,24 @@
+import uuid
+from datetime import datetime
 from django.db import models
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from users.models import User
+from orchestra.models import Orchestra
+
+class PracticeInfo(models.Model):
+
+    uuid = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    
+    title = models.CharField(_('タイトル'), max_length=300)
+    content = models.CharField(_('内容'), max_length=3000)
+    orchestra = ForeignKey(Orchestra, on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'PracticeInfo'
+        verbose_name = _('practice_info')
+        verbose_name_plural = _('練習に関するお知らせ')
+    
+    def get_full_name(self):
+        return self.title
