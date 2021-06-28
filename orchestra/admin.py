6ffd25ddceae3f2904489914d8b5c4d6793a5d7c
Hajime Fujita <hajime.fujita@gmail.com>
2021-06-28 16:31:45 +0900

    Orchestraモデルを少し修正した

--- orchestra/admin.py
+++ orchestra/admin.py
@@ -5,10 +5,10 @@ from django.utils.translation import gettext_lazy as _
 class OrchestraAdmin(admin.ModelAdmin):
 
     fieldsets = (
-        (_('Orchestra info'), {'fields': ('orchestra_name', 'email', 'admin_users', 'orchestra_type', 'birthday', 'postal_code', 'prefecture', 'address', 'building', 'tel', 'url', 'photo')}),
+        (_('Orchestra info'), {'fields': ('orchestra_name', 'email', 'admin_users', 'orchestra_type1', 'orchestra_type2', 'birthday', 'postal_code', 'prefecture', 'address', 'building', 'tel', 'url', 'photo')}),
     )
-    list_display = ('get_full_name', 'email', 'orchestra_type', 'prefecture', 'address', 'created_at',)
-    search_fields = ('orchestra_name', 'orchestra_name_kana', 'orchestra_type', 'email', 'prefecture', 'address',)
+    list_display = ('get_full_name', 'email', 'orchestra_type1', 'orchestra_type2', 'prefecture', 'address', 'created_at',)
+    search_fields = ('orchestra_name', 'orchestra_name_kana', 'orchestra_type1', 'orchestra_type2', 'email', 'prefecture', 'address',)
     ordering = ('created_at',)
 
 admin.site.register(Orchestra, OrchestraAdmin)
